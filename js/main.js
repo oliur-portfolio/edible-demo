1dfbe7d842ffaf94280242cc09e4cfa1a2552f82
Oliur Rahman <oliurr941@gmail.com>
2023-10-24 03:33:33 +0600

    responsive design

--- js/main.js
+++ js/main.js
@@ -6,12 +6,42 @@ $(".accordion__question").click(function (e) {
     $(this).toggleClass("active").next().slideToggle("fast");
 });
 
-// Animated Hamburger Icon
-const navToggleBtn = document.querySelector(".navbar-toggler");
-const header = document.querySelector(".header");
-
-navToggleBtn.addEventListener("click", () => {
-    navToggleBtn.classList.toggle("change");
-    header.classList.toggle("mobile-nav");
-    document.body.classList.toggle("overflow-hidden");
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 4,
+    spaceBetween: 20,
+    centeredSlides: false,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            slidesPerView: 0.5,
+            spaceBetween: 50,
+            centeredSlides: true,
+        },
+        400: {
+            slidesPerView: 1,
+            spaceBetween: 50,
+            centeredSlides: true,
+        },
+        767: {
+            slidesPerView: 1.35,
+            spaceBetween: 50,
+        },
+        991: {
+            slidesPerView: 1.9,
+            spaceBetween: 50,
+        },
+        // when window width is >= 480px
+        1200: {
+            slidesPerView: 2.7,
+            spaceBetween: 50,
+        },
+        // when window width is >= 640px
+        1750: {
+            slidesPerView: 4,
+        },
+    },
 });
